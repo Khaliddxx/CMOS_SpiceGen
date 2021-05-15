59c6e0a0bc491902e12c07757a3901c839557a6a
Khalid Ahmed <khalidxah@Khalids-MacBook-Air.local>
2021-05-15 11:57:38 +0200

    progress

--- Micro Project/MOSFET.cpp
+++ Micro Project/MOSFET.cpp
@@ -1,4 +1,5 @@
 #include "MOSFET.h"
+#include <sstream>
 #include <iostream>
 
 using namespace std;
@@ -26,6 +27,84 @@ MOSFET::MOSFET(string Mname, string drain, string gate, string source)
     
 //     // always vdd NET representing positive terminal of DC
 // }
+
+bool check_priority(char& operator1, char& operator2)
+{
+	bool priority_bool = false;
+
+	if ((operator1 == '`' && operator2 == '&') ||
+		(operator1 == '`' && operator2 == '|') ||
+		(operator1 == '&' && operator2 == '|'))
+		priority_bool = true;
+
+	return priority_bool;
+}
+
+vector <string> calculate_priority(string& input_exp)
+{
+	vector <string> string_vec;
+	vector <string> last_vec;
+	string current_str, expression;
+	char topchar;
+	std::stack<char> stack;
+	stringstream ss(input_exp);
+
+	while (ss.good())
+	{
+		string substr;
+		getline(ss, substr, ';');
+		string_vec.push_back(substr);
+	}
+
+	for (int i = 0; i < string_vec.size(); i++)
+	{
+		current_str = string_vec[i];
+		for (int i = 0; i < current_str.length(); i++)
+		{
+			if (current_str[i] == '`' || current_str[i] == '&' || current_str[i] == '|')
+			{
+				if (!stack.empty())
+					topchar = stack.top();
+				while (!stack.empty() && check_priority(topchar, current_str[i]))
+				{
+					topchar = stack.top();
+					stack.pop();
+					expression += topchar;
+					if (!stack.empty())
+						topchar = stack.top();
+				}
+				stack.push(current_str[i]);
+			}
+
+			else if (current_str[i] == '(')
+				stack.push(current_str[i]);
+
+			else if (current_str[i] == ')' && !stack.empty())
+			{
+				topchar = stack.top();
+				stack.pop();
+				while (!stack.empty() && topchar != '(')
+				{
+					expression += topchar;
+					topchar = stack.top();
+					stack.pop();
+				}
+			}
+			else
+				expression += current_str[i];
+		}
+
+		while (!stack.empty())
+		{
+			topchar = stack.top();
+			stack.pop();
+			expression += topchar;
+		}
+		last_vec.push_back(expression);
+		expression = "";
+	}
+	return last_vec;
+}
 
 
 void MOSFET::printout()
